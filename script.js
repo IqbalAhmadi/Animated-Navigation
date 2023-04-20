82c3593ce1cd07711431bb35e45eee57e6af10b4
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-20 09:19:52 -0700

    added js to activate the toggle

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
 
+// adding eventListener to the toggle
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active')
+})
